a40ac8bc0f8b95b612d3cabcb6d1e494984122fe
LisaPMunich <99111208+LisaPMunich@users.noreply.github.com>
2023-04-25 18:14:36 +0200

    create services and add them to directories in shared folder

---
+++ src/app/shared/directives/dropdown.directive.ts
@@ -0,0 +1,19 @@
+import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective implements OnInit {
+
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+  }
+
+  ngOnInit() {
+  }
+  @HostBinding('class.open') isOpen = false;
+
+  @HostListener('click')
+  toggleOpen(eventData: Event) {
+    this.isOpen = !this.isOpen;
+    }
+}
---
+++ src/app/shared/services/recipe.service.ts
@@ -0,0 +1,20 @@
+import {Recipe} from "../models/recipe.model";
+
+export class RecipeService {
+
+  constructor() { }
+
+  private recipes: Recipe[] = [
+    new Recipe('Vegetable Stir Fry', 'This healthy and delicious vegetable stir fry is quick and easy to make. It\'s packed with nutrients and perfect for a weeknight dinner.', 'https://images.pexels.com/photos/5908020/pexels-photo-5908020.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+    new Recipe('Chicken Tacos', 'These flavorful chicken tacos are perfect for a taco night. They\'re easy to make and taste amazing with fresh salsa and guacamole.', 'https://images.pexels.com/photos/14473870/pexels-photo-14473870.jpeg'),
+    new Recipe('Quinoa Salad with Avocado and Tomatoes','This quinoa salad with avocado and tomatoes is a healthy and flavorful side dish. It\'s packed with protein and perfect for a summer barbecue.','https://p1.pxfuel.com/preview/903/818/350/quinoa-salad-healthy.jpg'),
+    new Recipe('One-Pan Lemon Garlic Shrimp and Asparagus',
+      'This one-pan lemon garlic shrimp and asparagus recipe is easy to make and perfect for a quick weeknight dinner. It\'s healthy and packed with flavor.', 'https://images.pexels.com/photos/10875193/pexels-photo-10875193.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')
+  ];
+
+  getRecipes(){
+    return this.recipes.slice();
+  }
+
+
+}
